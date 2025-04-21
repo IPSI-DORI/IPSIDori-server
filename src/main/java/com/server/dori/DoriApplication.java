f6daa52c04cb4894bd81724d7ef5422ae527ac5f
조예설 <choyeseol07@gmail.com>
2025-04-21 18:31:12 +0900

    init

---
+++ src/main/java/com/server/dori/DoriApplication.java
@@ -0,0 +1,13 @@
+package com.server.dori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/dori/DoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.dori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
