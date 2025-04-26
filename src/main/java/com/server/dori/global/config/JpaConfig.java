b302e7bf4dfeb9ea5c9435fb90cbdc13a4de0bbe
박준규 <junju404@naver.com>
2025-04-27 02:22:51 +0900

    chore: JPA auditing 설정

--- src/main/java/com/server/dori/global/config/JpaConfig.java
+++ src/main/java/com/server/dori/global/config/JpaConfig.java
@@ -1,8 +1,9 @@
 package com.server.dori.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-// @EnableJpaAuditing
+@EnableJpaAuditing
 public class JpaConfig {
 }
