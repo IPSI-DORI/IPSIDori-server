378500b866070b930f819ed759de04d8f5bf2d4c
조예설 <choyeseol07@gmail.com>
2025-04-29 19:12:54 +0900

    test: DummyTest 작성

---
+++ src/test/java/com/server/dori/DummyTest.java
@@ -0,0 +1,16 @@
+package com.server.dori;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyTest {
+
+	@Test
+	void dummyTest() {
+		// 간단한 더미 테스트
+		assertTrue(true);
+	}
+}
