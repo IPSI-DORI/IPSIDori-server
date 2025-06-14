12031e4c27daf22ed3904cf4b5b608ca4d41c039
박준규 <junju404@naver.com>
2025-06-14 18:54:22 +0900

    feat: 이미지 url 필드 추가

--- src/main/java/com/server/dori/domain/member/presentation/dto/response/MemberInfoDetailResponseDto.java
+++ src/main/java/com/server/dori/domain/member/presentation/dto/response/MemberInfoDetailResponseDto.java
@@ -5,7 +5,7 @@ import com.server.dori.domain.member.entity.sub.Grade;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "회원 정보 상세 응답 DTO")
-public record MemberInfoResponseDto(
+public record MemberInfoDetailResponseDto(
 	@Schema(description = "회원 ID", example = "1")
 	Long memberId,
 
@@ -24,7 +24,7 @@ public record MemberInfoResponseDto(
 	@Schema(description = "희망 전공", example = "컴퓨터공학과")
 	String targetMajor,
 
-	@Schema(description = "캐릭터 이미지 URL", example = "https://ipsi-dori-character.s3.ap-northeast-2.amazonaws.com/dori.png")
+	@Schema(description = "캐릭터 이미지 URL", example = "https://xxx/xxx.png")
 	String characterImageUrl
 ) {
 }
