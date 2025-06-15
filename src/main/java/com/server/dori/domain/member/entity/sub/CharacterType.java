25089ffe81596a23c3530a234928d6fc198f0430
박준규 <junju404@naver.com>
2025-06-15 10:30:22 +0900

    fix: 학습 스타일 캐릭터 이미지 url 변경

--- src/main/java/com/server/dori/domain/member/entity/sub/CharacterType.java
+++ src/main/java/com/server/dori/domain/member/entity/sub/CharacterType.java
@@ -9,22 +9,22 @@ import lombok.Getter;
 
 @Getter
 public enum CharacterType {
-	NORI("노리", "기록형", 4, 6, "nori", Arrays.asList(
+	NORI("노리", "기록형", 4, 6, "nori", "노리", Arrays.asList(
 		"기록이 곧 기억! 정리하고 쓰면서 공부하는 타입이에요.",
 		"공부 내용을 스스로 정리하며 머릿속에 체계화해요.",
 		"손으로 쓰거나 정리할 때 더 잘 이해하고 기억해요."
 	)),
-	DORI("도리", "인강형", 7, 9, "dori", Arrays.asList(
+	DORI("도리", "인강형", 7, 9, "dori", "도리", Arrays.asList(
 		"인강으로 배우는 게 제일 잘 맞는 타입이에요.",
 		"강의 흐름을 따라가며 이해하고, 필요한 부분은 여러 번 돌려봐요.",
 		"듣고, 정리하고, 다시 듣기! 체계적인 복습 루틴이 있어요."
 	)),
-	MORI("모리", "암기형", 10, 12, "mori", Arrays.asList(
+	MORI("모리", "암기형", 10, 12, "mori", "모리", Arrays.asList(
 		"노트 필기나 암기 카드가 공부의 필수템이에요.",
 		"외워야 할 것과 버릴 것을 명확하게 구분해요.",
 		"'자주 보기'와 '반복하기'로 완벽하게 내 것으로 만들어요."
 	)),
-	LAGI("라기", "벼락치기형", 13, 16, "ragi", Arrays.asList(
+	LAGI("라기", "벼락치기형", 13, 16, "ragi", "라기", Arrays.asList(
 		"벼락치기의 천재, 단기간 집중력이 남다른 타입이에요.",
 		"마감 직전의 압박감 속에서 실력을 발휘해요.",
 		"'몰입할 때 제대로 몰입'하는 순간 집중형 스타일이에요."
@@ -34,16 +34,18 @@ public enum CharacterType {
 	private final String description;
 	private final int minScore;
 	private final int maxScore;
-	private final String imagePrefix;
+	private final String folderName;
+	private final String characterName;
 	private final List<String> characteristics;
 
-	CharacterType(String name, String description, int minScore, int maxScore, String imagePrefix,
+	CharacterType(String name, String description, int minScore, int maxScore, String folderName, String characterName,
 		List<String> characteristics) {
 		this.name = name;
 		this.description = description;
 		this.minScore = minScore;
 		this.maxScore = maxScore;
-		this.imagePrefix = imagePrefix;
+		this.folderName = folderName;
+		this.characterName = characterName;
 		this.characteristics = characteristics;
 	}
 
@@ -55,7 +57,7 @@ public enum CharacterType {
 	}
 
 	public String getImageUrl(String baseUrl) {
-		return String.format("%s/%s.png", baseUrl, this.imagePrefix);
+		return String.format("%s/%s/%s-1.png", baseUrl, this.folderName, this.characterName);
 	}
 
 	public static boolean isValidScore(int score) {
