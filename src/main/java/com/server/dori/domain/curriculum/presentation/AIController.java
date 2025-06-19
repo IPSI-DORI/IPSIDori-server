3a1e0513f8d102c9946d774b9d8134b7cfdba557
조예설 <choyeseol07@gmail.com>
2025-06-19 10:03:16 +0900

    chore: query문 -> AICurriculumRequest로 수정

--- src/main/java/com/server/dori/domain/curriculum/presentation/AIController.java
+++ src/main/java/com/server/dori/domain/curriculum/presentation/AIController.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
+import com.server.dori.domain.curriculum.entity.Curriculum;
 import com.server.dori.domain.curriculum.exception.ApiCallException;
+import com.server.dori.domain.curriculum.presentation.dto.request.AICurriculumRequest;
+import com.server.dori.domain.curriculum.repository.CurriculumRepository;
+import com.server.dori.domain.grade.entity.Grade;
+import com.server.dori.domain.grade.repository.GradeRepository;
+import com.server.dori.domain.member.entity.CustomUserDetails;
+import com.server.dori.domain.member.entity.Member;
+import com.server.dori.domain.member.entity.MemberInfo;
+import com.server.dori.domain.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,6 +33,9 @@ import lombok.RequiredArgsConstructor;
 public class AIController {
 
 	private final RestClient restClient;
+	private final MemberRepository memberRepository;
+	private final CurriculumRepository curriculumRepository;
+	private final GradeRepository gradeRepository;
 
 	@GetMapping
 	public ResponseEntity<String> getAIRoot() {
@@ -40,8 +53,16 @@ public class AIController {
 	}
 
 	@GetMapping("/curriculum")
-	public ResponseEntity<String> getAICurriculum(@RequestParam(name = "query") String query) {
+	public ResponseEntity<String> getAICurriculum(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestParam(name = "curriculumId") Long curriculumId) {
 		try {
+
+			MemberInfo memberInfo = memberRepository.getById(userDetails.getMemberId()).getMemberInfo();
+			Curriculum curriculum = curriculumRepository.getById(curriculumId);
+			Grade grade = gradeRepository.getById(curriculumId);
+
+			AICurriculumRequest request = new AICurriculumRequest(memberInfo, curriculum, grade);
+			String query = AICurriculumRequest.toQuery(request);
+
 			String response = restClient.get()
 				.uri(uriBuilder -> uriBuilder
 					.path("/curriculum")
