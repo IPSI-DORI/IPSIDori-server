71f28e5ae5c92f8798e6656c22d11612a199e4cf
조예설 <choyeseol07@gmail.com>
2025-06-19 10:02:32 +0900

    chore: GradeNotFoundException 수정

--- src/main/java/com/server/dori/domain/grade/exception/GradeNotFoundException.java
+++ src/main/java/com/server/dori/domain/grade/exception/GradeNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import com.server.dori.global.response.exception.CustomException;
 
 public class GradeNotFoundException extends CustomException {
-	public GradeNotFoundException(Long gradeId) {
-		super(HttpStatus.NOT_FOUND, "GRADE_404_1", "아이디가 " + gradeId + "인 성적을 찾을 수 없습니다.");
+	public GradeNotFoundException() {
+		super(HttpStatus.NOT_FOUND, "GRADE_404_1", "해당 성적을 찾을 수 없습니다.");
 	}
 }
