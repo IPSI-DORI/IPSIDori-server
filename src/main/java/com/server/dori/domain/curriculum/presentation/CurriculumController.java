688992a729540d9db56f09c0e6e12ccaaa9fefa3
조예설 <choyeseol07@gmail.com>
2025-06-20 10:55:42 +0900

    chore: API 구현체 분리

--- src/main/java/com/server/dori/domain/curriculum/presentation/CurriculumController.java
+++ src/main/java/com/server/dori/domain/curriculum/presentation/CurriculumController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.server.dori.domain.curriculum.presentation.api.CurriculumApi;
 import com.server.dori.domain.curriculum.presentation.dto.request.AICurriculumResponse;
 import com.server.dori.domain.curriculum.presentation.dto.request.CurriculumSurveyRequest;
 import com.server.dori.domain.curriculum.presentation.dto.response.CurriculumSurveyResponse;
@@ -16,23 +17,21 @@ import com.server.dori.domain.curriculum.service.CommandCuriculumService;
 import com.server.dori.domain.member.entity.CustomUserDetails;
 import com.server.dori.global.response.CustomApiResponse;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/curriculum")
-public class CurriculumController {
+public class CurriculumController implements CurriculumApi {
 	private final CommandCuriculumService commandCuriculumService;
 
-	@PostMapping("/survey")
+	@Override
 	public ResponseEntity<CustomApiResponse<CurriculumSurveyResponse>> saveSurvey(
 		@RequestBody CurriculumSurveyRequest request, @AuthenticationPrincipal CustomUserDetails userDetails) {
 		CurriculumSurveyResponse response = commandCuriculumService.saveSurvey(request, userDetails.getMemberId());
 		return CustomApiResponse.ok(response);
 	}
 
-	@GetMapping()
+	@Override
 	public ResponseEntity<CustomApiResponse<AICurriculumResponse>> getCurriculum(
 		@AuthenticationPrincipal CustomUserDetails userDetails,
 		@RequestParam(name = "curriculumId") Long curriculumId
