0bd74b8467d974fbca5a175bed6cf30909d7fd8d
조예설 <choyeseol07@gmail.com>
2025-06-27 09:57:05 +0900

    chore: 불필요한 import 삭제

--- src/main/java/com/server/dori/domain/grade/presentation/GradeController.java
+++ src/main/java/com/server/dori/domain/grade/presentation/GradeController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
