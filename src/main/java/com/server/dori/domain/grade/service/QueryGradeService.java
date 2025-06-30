f12c4ddf72d386164b72a6e89ddabf23ed8d4617
조예설 <choyeseol07@gmail.com>
2025-06-30 13:27:10 +0900

    chore: QueryService에 트랜잭션 추가

--- src/main/java/com/server/dori/domain/grade/service/QueryGradeService.java
+++ src/main/java/com/server/dori/domain/grade/service/QueryGradeService.java
@@ -3,6 +3,7 @@ package com.server.dori.domain.grade.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.server.dori.domain.grade.entity.Grade;
 import com.server.dori.domain.grade.presentation.dto.response.GradeResponse;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class QueryGradeService {
 
 	private final GradeReader gradeReader;
