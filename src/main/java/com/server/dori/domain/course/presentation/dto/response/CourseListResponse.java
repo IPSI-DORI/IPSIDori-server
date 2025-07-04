bf0ced4074ade7c2501f145939d07c55a47bcb5a
조예설 <choyeseol07@gmail.com>
2025-07-04 10:11:04 +0900

    chore: Dto 스웨거 어노테이션 추가

--- src/main/java/com/server/dori/domain/course/presentation/dto/response/CourseListResponse.java
+++ src/main/java/com/server/dori/domain/course/presentation/dto/response/CourseListResponse.java
@@ -1,14 +1,30 @@
 package com.server.dori.domain.course.presentation.dto.response;
 
 import com.server.dori.domain.course.entity.Course;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "강의 리스트 응답 DTO")
 public record CourseListResponse(
+
+	@Schema(description = "강의 Id", example = "1")
 	Long id,
+
+	@Schema(description = "학생 Id", example = "10")
 	Long creator,
+
+	@Schema(description = "강의 제목", example = "문학/비문학 종합 강의")
 	String title,
+
+	@Schema(description = "과목", example = "국어")
 	String subject,
+
+	@Schema(description = "대상 학년", example = "고1")
 	String grade,
+
+	@Schema(description = "플랫폼", example = "EBSi")
 	String platform,
+
+	@Schema(description = "가격", example = "0")
 	int price
 ) {
 	public static CourseListResponse of(Course course) {
--- src/main/java/com/server/dori/domain/course/presentation/dto/response/CourseResponse.java
+++ src/main/java/com/server/dori/domain/course/presentation/dto/response/CourseResponse.java
@@ -3,11 +3,21 @@ package com.server.dori.domain.course.presentation.dto.response;
 import java.util.List;
 
 import com.server.dori.domain.course.entity.Course;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "강의 상세 응답 DTO")
 public record CourseResponse(
+
+	@Schema(description = "강의 Id", example = "1")
 	Long id,
+
+	@Schema(description = "생성자 회원 Id", example = "10")
 	Long creator,
+
+	@Schema(description = "추천 강의 정보")
 	CourseDto recommendedCourse,
+
+	@Schema(description = "강의 목록")
 	List<LectureDto> lectureList
 ) {
 	public static CourseResponse of(Course course) {
@@ -37,19 +47,41 @@ public record CourseResponse(
 		);
 	}
 
+	@Schema(description = "추천 강의 DTO")
 	public record CourseDto(
+
+		@Schema(description = "강의 제목", example = "문학/비문학 종합 강의")
 		String title,
+
+		@Schema(description = "강의 설명", example = "문학과 비문학을 아우르는 종합 강의입니다.")
 		String description,
+
+		@Schema(description = "과목", example = "국어")
 		String subject,
+
+		@Schema(description = "강사명", example = "홍길동")
 		String teacher,
+
+		@Schema(description = "대상 학년", example = "고1")
 		String grade,
+
+		@Schema(description = "플랫폼", example = "EBSi")
 		String platform,
+
+		@Schema(description = "가격", example = "0")
 		int price,
+
+		@Schema(description = "추천 대상 설명", example = "문학에 약한 학생에게 추천")
 		String recommend
 	) {}
 
+	@Schema(description = "강의(lecture) DTO")
 	public record LectureDto(
+
+		@Schema(description = "강의 제목", example = "1강 OT")
 		String title,
+
+		@Schema(description = "강의 정보", example = "수업 내용에 대한 오리엔테이션입니다.")
 		String info
 	) {}
 }
