c409e53b88438fdd0ffb6920733ee3764c73d84a
박준규 <junju404@naver.com>
2025-07-20 15:45:48 +0900

    chore: 충돌 해결

--- src/main/java/com/server/dori/domain/auth/presentation/dto/request/KakaoLoginRequest.java
+++ src/main/java/com/server/dori/domain/auth/presentation/dto/request/KakaoLoginRequest.java
@@ -3,7 +3,7 @@ package com.server.dori.domain.auth.presentation.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record KakaoLoginRequest(
-	@NotBlank(message = "인가코드는 필수입니다.")
-	String authorizationCode
+	@NotBlank(message = "카카오 액세스 토큰은 필수입니다.")
+	String accessToken
 ) {
 }
--- src/main/java/com/server/dori/global/config/WebSecurityConfig.java
+++ src/main/java/com/server/dori/global/config/WebSecurityConfig.java
@@ -52,7 +52,6 @@ public class WebSecurityConfig {
 				// 인증
 				.requestMatchers(
 					"/api/v1/auth/oauth2/kakao",
-					"/api/v1/auth/signup",
 					"/api/v1/auth/reissue"
 				).permitAll()
 				.anyRequest()
